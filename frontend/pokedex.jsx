5787c0605c8ef39d63f266d590a864bb44082185
Jonathon Wright <jonwright93@gmail.com>
2017-03-08 15:35:32 -0800

    Add pokemon reducer and store

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as APIUtil from "./util/api_util";
 import {receiveAllPokemon} from './actions/pokemon_actions';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  const store = configureStore();
+  window.store = store;
+
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
   window.fetchAllPokemon = APIUtil.fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
