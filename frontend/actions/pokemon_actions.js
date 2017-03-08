7337b89061eebc6e77d9fad369fd7077ec6abe7b
Jonathon Wright <jonwright93@gmail.com>
2017-03-08 15:21:46 -0800

    Add first API query to fetch all pokemon

---
+++ frontend/actions/pokemon_actions.js
@@ -0,0 +1,7 @@
+import * as APIUtil from '../util/api_util';
+export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON';
+
+export const receiveAllPokemon = (pokemons) => ({
+  type: RECEIVE_ALL_POKEMON,
+  pokemons
+});
--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from "./util/api_util";
+import {receiveAllPokemon} from './actions/pokemon_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  window.fetchAllPokemon = APIUtil.fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
 });
