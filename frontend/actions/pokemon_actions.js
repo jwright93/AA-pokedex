45b8441cda90c3981d66fd31711c66d354286416
Jonathon Wright <jonwright93@gmail.com>
2017-03-09 12:16:03 -0800

    Add single pokemon actions

--- frontend/actions/pokemon_actions.js
+++ frontend/actions/pokemon_actions.js
@@ -1,5 +1,6 @@
 import * as APIUtil from '../util/api_util';
 export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON';
+export const RECEIVE_SINGLE_POKEMON = 'RECEIVE_SINGLE_POKEMON';
 
 export const receiveAllPokemon = (pokemons) => ({
   type: RECEIVE_ALL_POKEMON,
@@ -9,4 +10,14 @@ export const receiveAllPokemon = (pokemons) => ({
 export const requestAllPokemon = () => dispatch => {
   return APIUtil.fetchAllPokemon()
     .then(pokemon => dispatch(receiveAllPokemon(pokemon)));
+};
+
+export const receiveSinglePokemon = (pokemon) => ({
+  type: RECEIVE_SINGLE_POKEMON,
+  pokemon
+});
+
+export const requestSinglePokemon = (pokeId) => dispatch => {
+  return APIUtil.fetchSinglePokemon(pokeId)
+    .then(pokemon => dispatch(receiveSinglePokemon(pokemon)));
 };
--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import * as APIUtil from "./util/api_util";
 import configureStore from './store/store';
 import Root from './components/root';
+import {requestSinglePokemon} from './actions/pokemon_actions';
+import {requestAllPokemon} from './actions/pokemon_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   const store = configureStore();
   window.store = store;
+  window.requestSinglePokemon = requestSinglePokemon;
+  window.requestAllPokemon = requestAllPokemon;
 
   ReactDOM.render(<Root store={store}/>, rootEl);
   // ReactDOM.render(<h1>backup</h1>, rootEl);
