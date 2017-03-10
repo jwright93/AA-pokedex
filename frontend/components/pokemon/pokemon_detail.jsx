526e599e08a783feedb51b6f93f46b9084842ae7
Jonathon Wright <jonwright93@gmail.com>
2017-03-09 18:02:24 -0800

    add item images to pokemon

--- frontend/components/pokemon/pokemon_detail.jsx
+++ frontend/components/pokemon/pokemon_detail.jsx
@@ -15,13 +15,19 @@ class PokemonDetail extends React.Component {
     }
   }
 
+  formatItems(items) {
+    if (items) {
+      return items.map((item) => (
+        <li key={item.image_url}><img src={item.image_url}/></li>
+      ));
+    }
+  }
   // TODO: Join moves with commas
   render() {
     const poke = this.props.pokemonDetail;
-
     return (
       <div className="pokemonDetails">
-        <ul>
+        <ul className="details">
           <li>
             <img src={poke.image_url} />
           </li>
@@ -30,6 +36,9 @@ class PokemonDetail extends React.Component {
           <li>Attack: {poke.attack}</li>
           <li>Defense: {poke.defense}</li>
           <li>Moves: {poke.moves}</li>
+        </ul>
+        <ul className="items">
+          {this.formatItems(poke.items)}
         </ul>
         <br/>
       </div>
